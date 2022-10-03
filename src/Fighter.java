2f8f5445d4df52f6784ac6eaa91b463372bd4282
Furkan Ince <frknince77@gmail.com>
2022-10-03 09:25:24 +0300

    Initial commit

---
+++ src/Fighter.java
@@ -0,0 +1,18 @@
+public class Fighter {
+    String name;
+    int damage;
+    int health;
+    int weight;
+    double dodge;
+
+    Fighter(String name, int damage, int health, int weight){
+        this.damage = damage;
+        this.health = health;
+        this.name =  name;
+        this.weight = weight;
+        this.dodge = dodge;
+
+
+    }
+
+}
